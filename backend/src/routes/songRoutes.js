2e1cca9b52c524151b39648b20b93318d1b14ca4
joshua <condorenajoshua98@gmail.com>
2025-08-14 20:56:48 -0500

    se implemento el router canciones

--- backend/src/routes/songRoutes.js
+++ backend/src/routes/songRoutes.js
@@ -1,5 +1,14 @@
-const express=require('express')
-module.exports=routes=express()
-routes.get("/",(req,res)=>{
+//const express=require('express')
+import {Router} from "express"
+import { GetAllSongs, GetFeatureSongs, GetMadeForYou, GetTendingSongs } from "../controller/songController.js"
+import { ProtectRute } from "../middleware/auth.js"
+const router=Router()
+router.get("/",(req,res)=>{
     res.send("Welcome to Songs ")
 })
+router.use(ProtectRute)
+router.get("/g/songs",GetAllSongs)
+router.get("/g/songs/feature",GetFeatureSongs)
+router.get("/g/songs/made-for-you",GetMadeForYou)
+router.get("/g/songs/tending",GetTendingSongs)
+export default router
