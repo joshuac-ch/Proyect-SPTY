8e25b9601f2f8a456efe9882ad71514621cdb605
joshua <condorenajoshua98@gmail.com>
2025-08-15 21:01:52 -0500

    Se agregaron rutas al admin route

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,7 +1,16 @@
-const express=require('express')
-const { GetAdmin, createSong } = require('../controller/adminController')
-const { ProtectRute, requireAdmin } = require('../middleware/auth')
+//const express=require('express')
+//const { GetAdmin, createSong, deleteSong } = require('../controller/adminController')
+//const { ProtectRute, requireAdmin } = require('../middleware/auth')
+import {Router} from "express"
+import { ProtectRute,requireAdmin } from '../middleware/auth.js'
+import { CheckAdmin, GetAdmin,createAlbum,createSong,deleteAlbum,deleteSong } from '../controller/adminController.js'
+const router=Router()
+router.use(ProtectRute,requireAdmin)
+router.get("/",GetAdmin)
+router.get("/check",CheckAdmin)
+router.post("/create-song",createSong)
+router.delete("/delete/:id",deleteSong)
+router.post("/c/album",createAlbum)
+router.delete("d/album/:id",deleteAlbum)
 
-module.exports=routes=express()
-routes.get("/",GetAdmin)
-routes.post("/create-song",ProtectRute,requireAdmin,createSong)
+export default router
