83fc57c3ce5caa029b0538786f64d7b0c46d5898
joshua <condorenajoshua98@gmail.com>
2025-08-22 19:46:05 -0500

    Se agrego el comopnente glo FecthAlbumid

--- fronted/src/store/useMusicStore.tsx
+++ fronted/src/store/useMusicStore.tsx
@@ -1,28 +1,43 @@
-import React from 'react'
+
 import { create } from 'zustand'
 import { axiosInstance } from '../lib/axios'
+import type { Album, Song } from '../types';
 interface MusicStore{
-    songs:any[];
-    albums:any[];
+    songs:Song[];
+    albums:Album[];
     isLoading:boolean;
     error:string|null;
-    fetchAlbums:()=>Promise<void>
+    currentAlbum:Album|null
+    fetchAlbums:()=>Promise<void>;
+    fetchAbumsById:(id:string)=>Promise<void>;
 }
 export const useMusicStore=create<MusicStore>((set)=>({
   albums:[],
   songs:[],
   isLoading:false,
   error: null,
+  currentAlbum:null,
   fetchAlbums: async()=>{
     set({isLoading:true,error:null})
     try{
-        const response=await axiosInstance.get(`/users/g/users`)
+        const response=await axiosInstance.get(`/album/g/album`)
         set({albums:response.data})
 
-    }catch(error){
+    }catch(error:any){
         set({error: error.response.data.message})
     }finally{
         set({isLoading:false})
     }
-}
+    },
+    fetchAbumsById:async(id)=>{
+        set({isLoading:true,error:null})
+        try{
+            const response=await axiosInstance.get(`/album/g/album/${id}`)
+            set({currentAlbum:response.data})
+        }catch(error:any){
+            set({error:error.response.data.message})
+        }finally{
+            set({isLoading:false})
+        } 
+    },
 }))
