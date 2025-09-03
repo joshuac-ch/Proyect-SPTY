a2a3f3f6b97a72ca8267a0cadaca1b8ec66674f1
joshua <condorenajoshua98@gmail.com>
2025-09-02 19:35:08 -0500

    Se modifico el componente main.tsx

--- fronted/src/main.tsx
+++ fronted/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
+import Autprovider from './providers/Autprovider.tsx'
+// Import your Publishable Key
+const PUBLISHABLE_KEY = "pk_test_bWludC1kb3J5LTU3LmNsZXJrLmFjY291bnRzLmRldiQ"
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
+      <Autprovider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Autprovider>
+    </ClerkProvider>
   </StrictMode>,
 )
