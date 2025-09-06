e3cab18e38ccf18f225bc36a10369ddb1bee17fc
joshua <condorenajoshua98@gmail.com>
2025-09-05 19:31:44 -0500

    Se implemento el componente header en admin

---
+++ fronted/src/pages/admin/components/Header.tsx
@@ -0,0 +1,22 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <>
+    <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3 mb-8">
+            <Link to='/' className='rounded-lg'>
+                <img src="/spotify.png" className='size-10 text-black' alt="" />
+            </Link>
+            <div>
+                <h1 className='text-3xl font-bold'>Music Manager</h1>
+                <p className='text-zinc-400 mt-1'>Manage your music catalog</p>
+            </div>   
+        </div>
+        <UserButton></UserButton>
+    </div>
+    </>
+  )
+}
