ae54c3bba1c511dd35dffc0335fd758efbdeccdc
joshua <condorenajoshua98@gmail.com>
2025-09-06 20:26:08 -0500

    Se modifico el db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,10 @@
-const mongo=require('mongoose')
-const conectionMongo=async()=>{
+import mongoose from 'mongoose'
+export const conectionMongo=async()=>{
     try{
-        const con=await mongo.connect(process.env.MONGODB_CONNECTION)
+        const con=await mongoose.connect(process.env.MONGODB_CONNECTION)
         console.log(`Se realizo la conection con Mongo ${con.connection.host}`)
     }catch(err){
         console.error(err.message)
     }
 }
-module.exports={conectionMongo}
+//module.exports={conectionMongo}
