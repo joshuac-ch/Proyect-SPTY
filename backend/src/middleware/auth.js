e21c709f83483f80e2c19a7da71765840cd6cb9f
joshua <condorenajoshua98@gmail.com>
2025-09-07 19:45:38 -0500

    Se modifioc el middware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,14 +1,16 @@
 
 import { clerkClient } from '@clerk/express'
 export const ProtectRute=async(req,res,next)=>{
-    if(!req.auth.userId){
-        return res.stayus(401).json({message:"No autorizado"})
+    const auth=req.auth()
+    if(!auth?.userId){
+        return res.status(401).json({message:"No autorizado"})
     }
     next()
 }
 export const requireAdmin=async(req,res,next)=>{
     try{
-        const currenUser=await clerkClient.users.getUser(req.auth.userId)
+        const auth=req.auth()
+        const currenUser=await clerkClient.users.getUser(auth?.userId)
         const admin=currenUser.primaryEmailAddress.emailAddress==process.env.ADMIN_EMAIL     
         if(!admin){
             return res.status(403).json({message:"No eres admin"})
