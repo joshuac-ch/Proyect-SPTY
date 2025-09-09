a6dfbb084e1a4d1f236f389ac7f7ad884f2e4575
joshua <condorenajoshua98@gmail.com>
2025-09-08 19:31:42 -0500

    Se guardo topbar

--- fronted/src/components/TopBar.tsx
+++ fronted/src/components/TopBar.tsx
@@ -1,9 +1,11 @@
-import { SignedIn, SignedOut,  SignOutButton, UserButton } from '@clerk/clerk-react'
+import {SignedOut,  UserButton } from '@clerk/clerk-react'
 import { LayoutDashboardIcon } from 'lucide-react'
 
 import { Link } from 'react-router-dom'
 import SingOut from './SingOut'
 import { UseAuthStore } from '../store/useAuthStore'
+import { cn } from '../lib/utils'
+import { buttonVariants } from './ui/button'
 
 export const TopBar=()=>{
     const {isAdmin}=UseAuthStore()  
@@ -11,19 +13,21 @@ export const TopBar=()=>{
   return (
     <div className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10'>
         <div className='flex gap-2 items-center'>
+            <img src="/spotify.png" className='size-8' alt="spotify logo" />
             Spotify
         </div>
         <div className='flex items-center gap-4'>
             {isAdmin&&(
-                <Link to={"/admin"}>
+                <Link to={"/admin"} className={cn(buttonVariants({
+                    variant:"outline"
+                }))}>
                     <LayoutDashboardIcon className='size-4 mr-2'>
-                        Admin Dashboard
+                        
                     </LayoutDashboardIcon>
+                    Admin Dashboard
                 </Link>
-            )}           
-            <SignedIn>
-                <SignOutButton></SignOutButton>
-            </SignedIn>
+            )}      
+            
             
             <SignedOut>
                 <SingOut></SingOut>
