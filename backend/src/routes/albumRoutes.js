9b7d2f8c72e22958f2af0d4a82e006714486bb4b
joshua <condorenajoshua98@gmail.com>
2025-09-09 19:34:46 -0500

    Se modifico el album routes en las rutas

--- backend/src/routes/albumRoutes.js
+++ backend/src/routes/albumRoutes.js
@@ -1,5 +1,13 @@
-const express=require('express')
-module.exports=routes=express()
-routes.get("/",(req,res)=>{
+//const express=require('express')
+import {Router} from "express"
+import { GetAlbumAll, GetAlbumID } from "../controller/albumController.js"
+
+const router=Router()
+
+router.get("/",(req,res)=>{
     res.send("Welcome to Album ")
 })
+router.get("/g",GetAlbumAll)
+router.get("/g/:id",GetAlbumID)
+
+export default router
