9647954b154ac583192c4971323d7b41af4b6cff
joshua <condorenajoshua98@gmail.com>
2025-09-12 20:04:32 -0500

    Se modifico el controlador user

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,3 +1,4 @@
+import { Message } from "../models/messageModel.js";
 import { User } from "../models/userModel.js";
 export const GetAllUsers=async(req,res,next)=>{
     try{
@@ -7,6 +8,21 @@ export const GetAllUsers=async(req,res,next)=>{
         res.status(200).json(user)
     }catch(err){
         console.error(`Hubo un error ${err.message}`)
+        next(err)
+    }
+}
+export const GetMessage=async(req,res,next)=>{
+    try{
+        const myID=req.auth.userId 
+        const {userID}=req.params
+        const messages=await Message.find({
+            $or:[
+                {senderID:userID,recivedID:myID},
+                {senderID:myID,recivedID:userID}
+            ]
+        }).sort({createdAt:-1})
+        res.status(200).json(messages)
+    }catch(err){
         next(err)
     }
 }
