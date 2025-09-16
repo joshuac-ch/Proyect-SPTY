1253f1e46ff7932eb28bdb043f75ff68f0842076
joshua <condorenajoshua98@gmail.com>
2025-09-15 19:35:02 -0500

    Modificacion de la autenticacion

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,4 +1,7 @@
-const express=require("express")
-const { AuthCall } = require("../controller/authController")
-module.exports=routes=express()
-routes.post("/",AuthCall)
+import {Router} from "express"
+//const express=require("express")
+import { AuthCall } from "../controller/authController.js"
+//const { AuthCall } = require("../controller/authController")
+const router=Router()
+router.post("/",AuthCall)
+export default router
