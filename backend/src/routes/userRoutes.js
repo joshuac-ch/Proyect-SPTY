a8639d07fcb66dab0cc57a65e33da3f4dffbc2f4
joshua <condorenajoshua98@gmail.com>
2025-09-16 19:49:41 -0500

    Se implemento el userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,12 @@
-const express=require('express')
-module.exports=routes=express()
+import {Router} from "express"
+import { ProtectRute } from "../middleware/auth.js"
+import { GetAllUsers, GetMessage } from "../controller/userController.js"
+
+const routes=Router()
 routes.get("/",(req,res)=>{
     //req.auth.userId
     res.send("esta corriendo el servicio")
 })
+routes.get("/g",ProtectRute,GetAllUsers)
+routes.get("/message/:userID",ProtectRute,GetMessage)
+export default routes
