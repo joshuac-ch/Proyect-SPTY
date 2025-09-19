dcee3386eb387be49e3e63591c0884adbcc31e94
joshua <condorenajoshua98@gmail.com>
2025-09-19 18:52:37 -0500

    uploading de fronted

--- fronted/src/lib/axios.ts
+++ fronted/src/lib/axios.ts
@@ -2,4 +2,5 @@ import axios from "axios";
 export const axiosInstance=axios.create({
     baseURL:`/api`
     //baseURL:`http://13.222.174.90:5000/api`
+    //baseURL:`http://localhost:5000/api`
 })
--- fronted/src/main.tsx
+++ fronted/src/main.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from 'react-router-dom'
 import Autprovider from './providers/Autprovider.tsx'
 // Import your Publishable Key
-const PUBLISHABLE_KEY = "pk_test_bWludC1kb3J5LTU3LmNsZXJrLmFjY291bnRzLmRldiQ"
+const PUBLISHABLE_KEY = "pk_test_dml0YWwtcHJhd24tOTYuY2xlcmsuYWNjb3VudHMuZGV2JA"
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
