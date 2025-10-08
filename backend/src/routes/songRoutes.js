9cdcc8258a16a8f5abd3a6a6f904b47d4a90074d
joshua <condorenajoshua98@gmail.com>
2025-10-07 19:09:18 -0500

    Se implemento el GetGenero

--- backend/src/routes/songRoutes.js
+++ backend/src/routes/songRoutes.js
@@ -1,14 +1,18 @@
 //const express=require('express')
 import {Router} from "express"
-import { GetAllSongs, GetFeatureSongs, GetMadeForYou, GetTendingSongs } from "../controller/songController.js"
+import { GetAllSongs, GetFeatureSongs, GetGenero, GetMadeForYou, GetTendingSongs } from "../controller/songController.js"
 import { ProtectRute } from "../middleware/auth.js"
+import { EditSong, ShowSong } from "../controller/adminController.js"
 const router=Router()
 router.get("/",(req,res)=>{
     res.send("Welcome to Songs ")
 })
 //router.use(ProtectRute)
 router.get("/g/songs",GetAllSongs)
+router.put("/u/:id",EditSong)
 router.get("/g/features",GetFeatureSongs)
 router.get("/g/made-for-you",GetMadeForYou)
+router.get("/genero",GetGenero)
 router.get("/g/tendings",GetTendingSongs)
+router.get("/s/:id",ShowSong)
 export default router
