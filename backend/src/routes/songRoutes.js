cff5e30dfe67fa1ec619f9ce36b51c648f33a3f9
joshua <condorenajoshua98@gmail.com>
2025-10-07 19:37:41 -0500

    Se implementaron las rutas para generoSpecific y generoGlobal

--- backend/src/routes/songRoutes.js
+++ backend/src/routes/songRoutes.js
@@ -1,6 +1,6 @@
 //const express=require('express')
 import {Router} from "express"
-import { GetAllSongs, GetFeatureSongs, GetGenero, GetMadeForYou, GetTendingSongs } from "../controller/songController.js"
+import { GetAllSongs, GetFeatureSongs, GetGenero, GetMadeForYou, GetTendingSongs, GetTopGeneroSpecific } from "../controller/songController.js"
 import { ProtectRute } from "../middleware/auth.js"
 import { EditSong, ShowSong } from "../controller/adminController.js"
 const router=Router()
@@ -13,6 +13,7 @@ router.put("/u/:id",EditSong)
 router.get("/g/features",GetFeatureSongs)
 router.get("/g/made-for-you",GetMadeForYou)
 router.get("/genero",GetGenero)
+router.get("/top/genero/:genero",GetTopGeneroSpecific)
 router.get("/g/tendings",GetTendingSongs)
 router.get("/s/:id",ShowSong)
 export default router
