93c91160aa86c605dd42f813ee7e2ff35b4c5b12
joshua <condorenajoshua98@gmail.com>
2025-10-07 22:36:44 -0500

    Se implemento la funcion de featureSong

--- backend/src/controller/songController.js
+++ backend/src/controller/songController.js
@@ -13,25 +13,79 @@ export const GetAllSongs=async(req,res,next)=>{
 //Se pueden usar algoritmos para que no sean aleatorios
 export const GetFeatureSongs=async(req,res,next)=>{
 try{
-    const songs=await Song.aggregate([
-        {
-            $sample:{size:6}
-        },
-        {
-            $project:{
-                _id:1,
-                title:1,
-                artist:1,
-                imageURL:1,
-                audioURL:1
+    const topsongsGenero = await Reproducciones.aggregate([
+    {
+        $group:{
+            _id:"$song_ID",
+            total_plays:{$sum:"$plays"}
+        }
+    },
+    {
+        $lookup:{
+            from:"songs",
+            foreignField:"_id",
+            localField:"_id",
+            as:"song"
+        }
+    },
+    {
+        $unwind:"$song"
+    },
+    
+    {
+        $group: {
+        _id: "$song.genero",
+        total_plays: { $sum: "$plays" },
+        canciones: {
+            $push: {
+             _id:"$song._id",  
+            genero:"$song.genero",
+            title: "$song.title",
+            artist: "$song.artist",
+            imageURL: "$song.imageURL",
+            audioURL: "$song.audioURL",
+            videoURL: "$song.videoURL",
+            total_plays: "$total_plays"
             }
         }
-    ])
-    res.json(songs)    
-}catch(err){
-    console.error(err.message)
-    next(err)
-}
+        }
+    },
+    {
+            $project: {                
+            canciones: {
+                $slice: [
+                {
+                    $sortArray: {
+                    input: "$canciones",
+                    sortBy: { total_plays: -1 }
+                    }
+                },
+                1 // ← Cambia este número al top N que quieras
+                ]
+            }
+            }
+    },
+    {
+        $limit:6
+    },
+     { $unwind: "$canciones" },
+     {
+    $project: {
+      _id: "$canciones._id",
+      genero:"$canciones.genero",
+      title: "$canciones.title",
+      artist: "$canciones.artist",
+      imageURL: "$canciones.imageURL",
+      audioURL: "$canciones.audioURL",
+      videoURL: "$canciones.videoURL",
+      total_plays: "$canciones.total_plays"
+    }}
+    ]);
+    res.status(200).json(topsongsGenero)    
+    }catch(err){
+        console.error(err.message)
+        next(err)
+    }
 }
 export const GetMadeForYou=async(req,res,next)=>{
     try {
@@ -83,12 +137,126 @@ export const GetTendingSongs=async(req,res,next)=>{
                     artist:"$song.artist",
                     imageURL:"$song.imageURL",
                     audioURL:"$song.audioURL",
+                    videoURL:"$song.videoURL",
                     total_plays:1
                 }
             }
         ])
         
     res.json(tendencias)
+    }catch(err){
+        console.error(err.message)
+        next(err)
+    }
+}
+export const GetGenero=async(req,res,next)=>{
+    try{
+         const genero=await Song.distinct("genero")
+                     
+        if(!genero){
+            return res.status(404).json({message:"No se encontro el genero"})
+        }
+        res.status(200).json(genero)
+    }catch(err){
+        console.error(err.mssage)
+        next(err)
+    }
+}
+export const GetTopGeneroSpecific=async(req,res,next)=>{
+    try{
+        const {genero}=req.params
+        const topSongs=await Reproducciones.aggregate([
+            {
+                $group:{
+                    _id:"$song_ID",
+                    total_plays:{$sum:"$plays"}
+                }
+            },
+            {
+                $lookup:{
+                    from:"songs",
+                    foreignField:"_id",
+                    localField:"_id",
+                    as:"song"
+                }
+            },
+            {
+                $match:{
+                    "song.genero":genero
+                }
+            },
+            {
+                $sort:{total_plays:-1}
+            },
+            {
+                $limit:3
+            },           
+            {
+                $unwind:"$song"
+            },
+
+        ])
+        res.status(200).json(topSongs)
+    }catch(err){
+        console.error(err.message)
+        next(err)
+    }
+}
+export const GetTopGeneroGlobal=async(req,res,next)=>{
+  try{
+    const topsongsGenero = await Reproducciones.aggregate([
+    {
+        $group:{
+            _id:"$song_ID",
+            total_plays:{$sum:"$plays"}
+        }
+    },
+    {
+        $lookup:{
+            from:"songs",
+            foreignField:"_id",
+            localField:"_id",
+            as:"song"
+        }
+    },
+    { $unwind: "$song" },
+    
+    {
+        $group: {
+        _id: "$song.genero",
+        total_plays: { $sum: "$plays" },
+        canciones: {
+            $push: {
+            title: "$song.title",
+            artist: "$song.artist",
+            imageURL: "$song.imageURL",
+            audioURL: "$song.audioURL",
+            videoURL: "$song.videoURL",
+            total_plays: "$total_plays"
+            }
+        }
+        }
+    },
+    {
+            $project: {                
+            canciones: {
+                $slice: [
+                {
+                    $sortArray: {
+                    input: "$canciones",
+                    sortBy: { total_plays: -1 }
+                    }
+                },
+                1 // ← Cambia este número al top N que quieras
+                ]
+            }
+            }
+    },
+    {
+        $limit:6
+    }
+    ]);
+    res.status(200).json(topsongsGenero)    
     }catch(err){
         console.error(err.message)
         next(err)
